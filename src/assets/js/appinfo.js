6672624bf942d9e0392a8ebe89c3fd842625383a
tassun <32536248+tassun@users.noreply.github.com>
2024-07-20 11:13:19 +0700

    support raw parameter setting

--- src/assets/js/appinfo.js
+++ src/assets/js/appinfo.js
@@ -2,15 +2,14 @@ var DEFAULT_LANGUAGE = process.env.VUE_APP_DEFAULT_LANGUAGE ? process.env.VUE_AP
 var API_URL = process.env.VUE_APP_API_URL;
 var BASE_URL = process.env.VUE_APP_BASE_URL;
 var CDN_URL = process.env.VUE_APP_CDN_URL;
+var IMG_URL = process.env.VUE_APP_IMG_URL;
 var BASE_STORAGE = process.env.VUE_APP_BASE_STORAGE;
-var API_TOKEN;
-var DEFAULT_RAW_PARAMETERS = false;
+var API_TOKEN = process.env.VUE_APP_API_TOKEN;
+var DEFAULT_RAW_PARAMETERS = process.env.VUE_APP_DEFAULT_RAW_PARAMETERS == "true";
 export const DEFAULT_CONTENT_TYPE = "application/x-www-form-urlencoded; charset=UTF-8";
-console.log("DEFAULT_LANGUAGE="+DEFAULT_LANGUAGE);
-console.log("API_URL="+API_URL);
-console.log("BASE_URL="+BASE_URL);
-console.log("CDN_URL="+CDN_URL);
-console.log("BASE_STORAGE="+BASE_STORAGE);
+console.info("DEFAULT_LANGUAGE="+DEFAULT_LANGUAGE,", BASE_STORAGE="+BASE_STORAGE,", DEFAULT_RAW_PARAMETERS="+DEFAULT_RAW_PARAMETERS);
+console.info("API_URL="+API_URL,", BASE_URL="+BASE_URL,", CDN_URL="+CDN_URL,", IMG_URL="+IMG_URL);
+console.info("API_TOKEN="+API_TOKEN);
 export function getDefaultLanguage() { return DEFAULT_LANGUAGE; }
 export function setDefaultLanguage(language) {
 	console.log("set default_language="+language);
@@ -20,11 +19,13 @@ export function getApiToken() { return API_TOKEN; }
 export function getApiUrl() { return API_URL; }
 export function getBaseUrl() { return BASE_URL; }
 export function getCdnUrl() { return CDN_URL; }
+export function getImgUrl() { return IMG_URL; }
 export function getBaseStorage() { return BASE_STORAGE; }
 export function getDefaultRawParameters() { return DEFAULT_RAW_PARAMETERS; }
 export function setApiToken(value) { API_TOKEN = value; }
 export function setApiUrl(value) { API_URL = value; }
 export function setBaseUrl(value) { BASE_URL = value; }
 export function setCdnUrl(value) { CDN_URL = value; }
+export function setImgUrl(value) { IMG_URL = value; }
 export function setBaseStorage(value) { BASE_STORAGE = value; }
 export function setDefaultRawParameters(value) { DEFAULT_RAW_PARAMETERS = value; }
